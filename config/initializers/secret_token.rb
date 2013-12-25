e176ff095c70dfed9a8cca63d6900474cd5976e9
Brandon Cato <b.cato28@gmail.com>
2013-12-25 12:21:36 -0800

    push practice app to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeTodo::Application.config.secret_key_base = '760669588a82531d5064aab57d3510f3792ab472f3b34b657af55e6bd09c67298c238bd242d21a193a948d3f324a7a0da89095fd5effffe3fe737b8e6fda5479'
